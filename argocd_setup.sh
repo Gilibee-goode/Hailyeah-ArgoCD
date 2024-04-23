0c67fffbca586c8b110475ef7b98ed4bf79b1a3b
Gilibee <gilig42@gmail.com>
2024-04-23 23:03:30 +0300

    ArgoCD installation doc and script edit

---
+++ argocd_setup.sh
@@ -0,0 +1,19 @@
+#follow these steps to set up ArgoCD on a local minikube
+
+kubectl create namespace argocd
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+
+
+
+kubectl port-forward svc/argocd-server -n argocd 8080:443
+
+#get admin password
+argocd admin initial-password -n argocd
+
+#log in with user "admin" and password from the previous step
+argocd login localhost:8080
+
+#set up the application
+argocd app create hailyeah -f Hailyeah_app.yaml 
+
+
